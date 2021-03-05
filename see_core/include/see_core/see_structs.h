2473aa10d9cd3a2f77b3dda4318399354728e334
Rowan Border <rowanjborder@gmail.com>
2021-03-05 10:50:37 -0400

    SEE public push

---
+++ see_core/include/see_core/see_structs.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#ifndef SEE_STRUCTS_H
+#define SEE_STRUCTS_H
+
+#include <string>
+#include <vector>
+
+#include <see_common/common_structs.h>
+
+//! SEE(++) Parameters
+/*! Parameters for SEE(++) */
+struct SeeParams : AlgParams {
+  int tau;   /*!< Maximum number of frontiers to update for a single view */
+  float psi; /*!< The search radius used for detecting occlusions */
+  float ups; /*!< The search radius used for evaluating visibility */
+};
+
+#endif // SEE_STRUCTS_H
